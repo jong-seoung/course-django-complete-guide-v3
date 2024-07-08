9b9426ee990509aeccefbfebc4375ee0b7538060
beak jong seoung <jjong015189@gmail.com>
2024-07-08 15:11:50 +0900

    DatePickerField 폼 필드를 위한 min_value, max_value 유효성 검사 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,10 +3,14 @@ import datetime
 from django import forms
 from .models import Profile
 
-from core.forms.fields import PhoneNumberField
+from core.forms.fields import PhoneNumberField, DatePickerField
 from core.forms.widgets import PhoneNumberInput, DatePickerInput, DatePickerOptions
 
 class ProfileForm(forms.ModelForm):
+    mydate = DatePickerField(
+        min_value=lambda: datetime.date.today(),
+        max_value=lambda: datetime.date.today() + datetime.timedelta(days=7),
+    )
     # phone_number = PhoneNumberField()
 
     def __init__(self, *args, **kwargs):
