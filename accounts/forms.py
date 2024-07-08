14778c5c3e0e12acd005019e87168d345e56459b
beak jong seoung <jjong015189@gmail.com>
2024-07-08 14:50:41 +0900

    vanilla-js-datepicker 라이브러리를 적용한 DatePickerInput 위젯, accounts.Profile…

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Profile
 
 from core.forms.fields import PhoneNumberField
-from core.forms.widgets import PhoneNumberInput
+from core.forms.widgets import PhoneNumberInput, DatePickerInput
 
 class ProfileForm(forms.ModelForm):
     # phone_number = PhoneNumberField()
@@ -14,10 +14,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = [
+            "birth_date",
             "address",
             "phone_number",
             "photo",
         ]
         widgets = {
+            "birth_date": DatePickerInput,
             "phone_number": PhoneNumberInput,
         }
