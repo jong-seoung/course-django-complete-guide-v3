131eddc969d0a983c894d8b996012bedeec5bb19
beak jong seoung <jjong015189@gmail.com>
2024-07-08 14:44:22 +0900

    3자리 전화번호 PhoneNumberInput 위젯 및 accounts.Profile 모델에 phone_number 필드 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from .models import Profile
 
+from core.forms.fields import PhoneNumberField
+from core.forms.widgets import PhoneNumberInput
 
 class ProfileForm(forms.ModelForm):
+    phone_number = PhoneNumberField()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["address"].required = True
 
     class Meta:
         model = Profile
-        fields = ["address", "photo"]
+        fields = [
+            "address",
+            # "phone_number",
+            "photo",
+        ]
+        widgets = {
+            # "phone_number": PhoneNumberInput,
+        }
