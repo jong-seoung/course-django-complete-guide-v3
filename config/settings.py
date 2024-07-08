e8baa8a8f3253da5a5f22afd06f316d887e4da33
beak jong seoung <jjong015189@gmail.com>
2024-07-08 12:41:01 +0900

    장고 위젯 템플릿 캐싱 끄기

--- config/settings.py
+++ config/settings.py
@@ -166,6 +166,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+# django form
+
+if DEBUG:
+    FORM_RENDERER = "core.forms.renderers.NoCacheDjangoTemplates"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
