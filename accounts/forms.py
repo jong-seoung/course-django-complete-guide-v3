bb31ed2865779f6e9f0f3cd81095d7d4c5da4aa3
beak jong seoung <jjong015189@gmail.com>
2024-07-11 13:03:47 +0900

    암호 재설정 폼과 뷰를 직접 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,26 @@
 import datetime
+from typing import Iterator
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.password_validation import validate_password
-from .models import Profile
+from django.contrib.auth.tokens import default_token_generator
+from django.http import HttpRequest
+from django.shortcuts import resolve_url
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
-from core.forms.fields import PhoneNumberField, DatePickerField
-from core.forms.widgets import PhoneNumberInput, DatePickerInput, DatePickerOptions, NaverMapPointInput
+# from core.forms.fields import PhoneNumberField, DatePickerField
+from core.forms.widgets import (
+    PhoneNumberInput,
+    DatePickerInput,
+    DatePickerOptions,
+    NaverMapPointInput,
+)
 from .models import Profile, User
+
+
+token_generator = default_token_generator
 
 
 class UserForm(forms.ModelForm):
@@ -15,18 +28,18 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ["first_name", "last_name"]
 
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ["address", "phone_number", "photo"]
-
     is_profile_update = forms.BooleanField(
         required=False,  # 체크하지 않아도 유효성 검사에 통과하기
         initial=True,
         label="프로필 수정 여부",
         help_text="체크 해제하시면 프로필 수정 단계를 생략합니다.",
     )
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["address", "phone_number", "photo"]
 
 
 class ProfileUserForm(forms.ModelForm):
@@ -42,13 +55,14 @@ class ProfileUserForm(forms.ModelForm):
             if qs.exists():
                 raise forms.ValidationError("이미 등록된 이메일입니다.")
         return email
-    
-    
+
+
 class ProfileForm(forms.ModelForm):
     # mydate = DatePickerField(
     #     min_value=lambda: datetime.date.today(),
     #     max_value=lambda: datetime.date.today() + datetime.timedelta(days=7),
     # )
+
     # phone_number = PhoneNumberField()
 
     def __init__(self, *args, **kwargs):
@@ -67,11 +81,9 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             "birth_date": DatePickerInput(
                 date_picker_options=DatePickerOptions(
-                    # datesDisabled=lambda: [
-                    #     datetime.date.today() + datetime.timedelta(days=2),
-                    # ],
-                    minDate=lambda: datetime.date.today(),
-                    maxDate=lambda: datetime.date.today() + datetime.timedelta(days=7),
+                    datesDisabled=lambda: [
+                        datetime.date.today() + datetime.timedelta(days=2),
+                    ],
                     todayButton=True,
                     todayHighlight=True,
                 ),
@@ -118,3 +130,41 @@ class PasswordChangeForm(forms.Form):
         if commit:
             self.user.save()
         return self.user
+
+
+class PasswordResetForm(forms.Form):
+    # 이메일 포맷에 대한 유효성 검사만 수행할 뿐, 이메일의 존재 유무를 확인하지는 않습니다.
+    email = forms.EmailField()
+
+    # auth앱의 PasswordResetForm에서는 save 메서드에서 이메일 발송에 필요한
+    # 다양한 인자를 전달받습니다.
+    def save(self, request: HttpRequest) -> None:
+        email = self.cleaned_data.get("email")
+        for uidb64, token in self.make_uidb64_and_token(email):
+            scheme = "https" if request.is_secure() else "http"
+            host = request.get_host()
+            # 새로운 암호를 입력받아, 암호를 변경하는 뷰
+            path = resolve_url(
+                "accounts:password_reset_confirm", uidb64=uidb64, token=token
+            )
+            reset_url = f"{scheme}://{host}{path}"
+            print(
+                f"{email} 이메일로 {reset_url} 주소를 발송합니다."
+            )  # TODO: 이메일 발송
+
+    def make_uidb64_and_token(self, email: str) -> Iterator[tuple[str, str]]:
+        for user in self.get_users(email):
+            print(f"{email}에 매칭되는 유저를 찾았습니다.")
+
+            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+            token = token_generator.make_token(user)
+
+            yield uidb64, token
+
+    def get_users(self, email: str) -> Iterator[User]:
+        active_users = User.objects.filter(email__iexact=email, is_active=True)
+        return (
+            user
+            for user in active_users
+            if user.has_usable_password() and email == user.email
+        )
