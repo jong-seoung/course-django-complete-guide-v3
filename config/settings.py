42ab115a2afa85bedd14c3bf5be2a88818c90166
beak jong seoung <jjong015189@gmail.com>
2024-07-12 15:12:19 +0900

    django-htmx 라이브러리 적용

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "django.forms",
     "django_bootstrap5",
+    "django_htmx",
     'core',
     'hottrack',
     'blog',
@@ -77,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 if DEBUG:
