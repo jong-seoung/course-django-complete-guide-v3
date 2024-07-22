03fbc3e608f1aae8238abba39271b618364765d8
beak jong seoung <jjong015189@gmail.com>
2024-07-22 12:49:57 +0900

    프로필 페이지 구현

--- mypro/accounts/urls.py
+++ mypro/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, profile
 
 app_name = "accounts"
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("profile/", profile, name="profile"),
 ]
--- mypro/accounts/views.py
+++ mypro/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.shortcuts import render
 
 from accounts.forms import LoginForm
 
@@ -9,3 +11,8 @@ class LoginView(DjangoLoginView):
     extra_context = {
         "form_title": "로그인",
     }
+
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html")
