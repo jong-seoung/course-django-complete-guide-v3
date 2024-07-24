8eebe35f769f7758be06b6fa162f9552ed169125
beak jong seoung <jjong015189@gmail.com>
2024-07-24 15:20:48 +0900

    note edit 페이지 - 기본 구현, 사진 삭제는 미구현

--- mypro/photolog/urls.py
+++ mypro/photolog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, NoteCreateView, NoteDetailView
+from .views import index, note_edit, NoteCreateView, NoteDetailView
 
 app_name = "photolog"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("new/", NoteCreateView.as_view(), name="note_new"),
     path("<int:pk>/", NoteDetailView.as_view(), name="note_detail"),
+    path("<int:pk>/edit/", note_edit, name="note_edit"),
 ]
