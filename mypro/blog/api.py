a1f2c334767bad88ba33359f71f941112808b202
beak jong seoung <jjong015189@gmail.com>
2024-08-08 16:47:01 +0900

    Serializer를 활용환 AIP 구현

---
+++ mypro/blog/api.py
@@ -0,0 +1,45 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
+
+from .models import Post
+from .serializers import PostSerializer, PostListSerializer, PostDetailSerializer
+
+
+# @api_view(["GET"])
+# def post_list(request: Request) -> Response:
+#     post_qs = Post.objects.all().defer("content").select_related("author")
+#
+#     serializer = PostListSerializer(instance=post_qs, many=True)
+#     list_data: ReturnList = serializer.data
+#
+#     return Response(list_data)
+
+
+class PostListAPIView(ListAPIView):
+    queryset = PostListSerializer.get_optimized_queryset()
+    serializer_class = PostListSerializer
+
+
+post_list = PostListAPIView.as_view()
+
+
+# @api_view(["GET"])
+# def post_detail(request: Request, pk: int) -> Response:
+#     post = get_object_or_404(Post, pk=pk)
+#
+#     serializer = PostDetailSerializer(instance=post)
+#     detail_data: ReturnDict = serializer.data
+#
+#     return Response(detail_data)
+
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = PostDetailSerializer.get_optimized_queryset()
+    serializer_class = PostDetailSerializer
+
+
+post_detail = PostRetrieveAPIView.as_view()
--- mypro/blog/urls.py
+++ mypro/blog/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from . import api
 
 urlpatterns = []
+
+urlpatterns_api_v1 = [
+    path("", api.post_list, name="post_list"),
+    path("<int:pk>/", api.post_detail, name="post_detail"),
+]
+
+urlpatterns += [
+    path("api/", include((urlpatterns_api_v1, "api-v1"))),
+]
