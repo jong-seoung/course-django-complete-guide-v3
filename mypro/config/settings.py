0cdba8aac2fd5b276f951831b2c17c330ad14430
beak jong seoung <jjong015189@gmail.com>
2024-08-08 12:54:55 +0900

    SENTRY 라이브러리 적용

--- mypro/config/settings.py
+++ mypro/config/settings.py
@@ -207,6 +207,23 @@ else:
         EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
+# sentry
+
+SENTRY_DSN = env.str("SENTRY_DSN", default="")
+if SENTRY_DSN:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=1.0),
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=env.float("SENTRY_PROFILES_SAMPLE_RATE", default=1.0),
+        )
+
 # django-components
 #  - context variable를 resolve하는 방식이 변경
 #    https://github.com/EmilStenstrom/django-components/?tab=readme-ov-file#isolate-components-slots
