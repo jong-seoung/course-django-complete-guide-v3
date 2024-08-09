d66a9e554e4aecb8d12e973cf00253022b620265
beak jong seoung <jjong015189@gmail.com>
2024-08-09 10:47:17 +0900

    auth 앱의 UserAdmin을 accounts앱에서 활용

--- mypro/accounts/admin.py
+++ mypro/accounts/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    pass
