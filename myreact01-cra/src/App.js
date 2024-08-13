bbd2688b5cb2456b226c07db4def3be784396ae8
beak jong seoung <jjong015189@gmail.com>
2024-08-13 15:20:44 +0900

    선언적 UI, Counter 컴포넌트 예시 추가

--- myreact01-cra/src/App.js
+++ myreact01-cra/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+function Counter( {initialCount} ){
+  const [count, setCount] = useState(initialCount);
+
+  return (
+    <button onClick={()=> setCount(count + 1)}>{count}</button>
+  )
+}
 
 function App() {
+  const style = {margin: "1em"};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"} style={style}>
+      <Counter initialCount={10} />
+      <Counter initialCount={12} />
+      <Counter initialCount={13} />
     </div>
   );
 }
