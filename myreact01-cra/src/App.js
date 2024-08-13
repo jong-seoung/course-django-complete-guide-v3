b77883676e290fc89405093075a4994dccab49e0
beak jong seoung <jjong015189@gmail.com>
2024-08-13 15:42:39 +0900

    [Counter 컴포넌트] 초기값, 1 증가/감소 루틴에 최댓값/최소값 적용

--- myreact01-cra/src/App.js
+++ myreact01-cra/src/App.js
@@ -4,15 +4,19 @@ function Counter({ initialCount }) {
   const [count, setCount] = useState(initialCount);
 
   const increment = () => {
-    console.log("increment", "count=", count);
-    // setCount(count + 1);
-    // setCount(count + 1);
-    setCount(prevCount => prevCount + 1);
-    setCount(prevCount => prevCount + 1);
+    setCount((prevCount) => {
+      if (prevCount >= 10) return 10;
+        return prevCount + 1;
+    });
   };
+
   const decrement = () => {
-    setCount(prevCount => prevCount - 1);
+    setCount((prevCount) => {
+    if (prevCount <= 0) return 0;
+    return prevCount - 1;
+    });
   };
+
   return (
     <button
       onClick={() => increment()}
@@ -32,8 +36,8 @@ function App() {
   return (
     <div className={"App"} style={style}>
       <Counter initialCount={10} />
-      <Counter initialCount={12} />
-      <Counter initialCount={13} />
+      <Counter initialCount={20} />
+      <Counter initialCount={30} />
     </div>
   );
 }
