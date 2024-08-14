b83f26b97c8cca41af4065097dbf3c95a09efe65
beak jong seoung <jjong015189@gmail.com>
2024-08-14 10:42:58 +0900

    입력 글자수를 보여주는 TextInput 컴포넌트 구현

--- myreact01-cra/src/App4.js
+++ myreact01-cra/src/App4.js
@@ -1,20 +1,24 @@
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
-}
+import TextInput from "./components/TextInput";
+
+// function Button({ onClick, children }) {
+//   return <button onClick={onClick}>{children}</button>;
+// }
 
 function App() {
-  const handleClick = (e) => {
-    console.log("clicked", e?.target);
-  };
+//   const handleClick = (e) => {
+//     console.log("clicked", e?.target);
+//   };
 
   return (
     <div>
-      <button onClick={(e) => handleClick(e)}>
+        <TextInput name={"username"}></TextInput>
+        <TextInput name={"email"}></TextInput>
+      {/* <button onClick={(e) => handleClick(e)}>
         엘리먼트 버튼
       </button>
       <Button onClick={() => console.log("clicked element")}>
         컴포넌트 버튼
-      </Button>
+      </Button> */}
     </div>
   );
 }
