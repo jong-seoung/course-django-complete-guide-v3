10f4fb1e67d83ea81a84d4db532b598e1ce92b2f
beak jong seoung <jjong015189@gmail.com>
2024-08-20 11:09:34 +0900

    react-router-dom을 통한 페이지 이동

---
+++ myreact01-cra/src/App8.jsx
@@ -0,0 +1,44 @@
+import {
+    createBrowserRouter,
+    NavLink,
+    Outlet,
+    RouterProvider,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <ul>
+          <li>
+            <NavLink to="/">홈</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog">블로그</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">소개</NavLink>
+          </li>
+        </ul>
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <div>Home</div> },
+      { path: "blog", element: <div>Blog</div> },
+      { path: "about", element: <div>About</div> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <h2>라우터</h2>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
