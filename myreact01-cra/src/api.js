59bbebeeb722fa3869e3f6e0b877e89497a81a9b
beak jong seoung <jjong015189@gmail.com>
2024-08-20 15:24:58 +0900

    CRA 리액트 페이지에서 세션 인증을 활용한 HTTP 요청 장고에서 필요한 환경변수
    CORS_ALLOWED_ORIGINS=http://localhost:3000
    CORS_ALLOW_CREDENTIALS=true
    
    리액트에서 필요한 환경변수
    
    REACT_APP_API_HOST=http://localhost:8000
    REACT_APP_API_WITH_CREDENTIALS=true

--- myreact01-cra/src/api.js
+++ myreact01-cra/src/api.js
@@ -1,8 +1,11 @@
 import Axios from "axios";
 import { makeUseAxios } from "axios-hooks";
+import { API_HOST, API_TIMEOUT, API_WITH_CREDENTIALS } from "./constants";
 
 const axiosInstance = Axios.create({
-  baseURL: "https://pyhub.kr",
+  baseURL: API_HOST,
+  timeout: API_TIMEOUT,
+  withCredentials: API_WITH_CREDENTIALS,
   // headers: {
   //   "Content-Type": "application/json",
   // },
