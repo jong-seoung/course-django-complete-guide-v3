6352a0627a9a57bdc29bc8b823d576fc203c6dd9
beak jong seoung <jjong015189@gmail.com>
2024-08-20 17:13:56 +0900

    장고, 유저 status API 구현

--- mypro/accounts/urls.py
+++ mypro/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import LoginView, profile, LogoutView, SignupView, ProfileUpdateView
+from . import api
 
 app_name = "accounts"
+
+urlpatterns_api_v1 = [
+    path("status/", api.status, name="status"),
+]
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
