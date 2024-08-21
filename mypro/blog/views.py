208fbb8282a844f44898ba20c0642074c5fb4877
beak jong seoung <jjong015189@gmail.com>
2024-08-21 15:29:31 +0900

    blog:index 뷰에서 포스팅 목록 HTML을 반환토록 하고, cache_page 장식자 적용

--- mypro/blog/views.py
+++ mypro/blog/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from django_nextjs.render import render_nextjs_page
+
+from blog.models import Post
+
+
+@cache_page(600)
+def index(request):
+    post_qs = Post.objects.all()
+    return render(
+        request,
+        "blog/index.html",
+        {
+            "post_list": post_qs,
+        },
+    )
 
 
 def whoami(request):
     status = 200 if request.user.is_authenticated else 401
     username = request.user.username or "anonymous"
     return HttpResponse(f"Your username is <strong>{username}</strong>.", status=status)
-
-
-async def index(request):
-    # return render(request, "blog/index.html")
-    return await render_nextjs_page(
-        request,
-        template_name="blog/index.html",
-        context={},
-    )
